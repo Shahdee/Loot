c266ba4e2082671d50cd0105de9a7b5fb64471e5
DESKTOP-J71ODHN\Colikoffv <13thshadow@gmail.com>
2024-08-22 11:44:56 +0300

    First commit

---
+++ Assets/Scripts/PlayerAsset.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="PlayerAsset", menuName="SO / PlayerAsset", order = 1)]
+public class PlayerAsset : ScriptableObject
+{
+    public PlayerData PlayerData => _playerData;
+    
+    [SerializeField] private PlayerData _playerData;
+}
---
+++ Assets/Scripts/Windows/EWindowType.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EWindowType 
+{
+   Main,
+   Drop,
+   ItemStat,
+}
